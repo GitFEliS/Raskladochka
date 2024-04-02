189696099b6a79c545e009aa62800bdd8ef51237
andreyvaran <moscow.russia.mai@gmail.com>
2024-04-02 22:11:25 +0300

    simple bot

---
+++ config_reader.py
@@ -0,0 +1,12 @@
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    bot_token: SecretStr
+    payment_token: str
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8' ,extra="ignore")
+
+
+config = Settings()
+print(config.payment_token)
